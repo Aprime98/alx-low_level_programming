e27255c19aa39e532466bdcee7e1960caee985ca
Charles Mwangi <bsclmr696619@spu.ac.ke>
2023-10-11 09:11:35 -0400

    add first nested

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,22 @@
 #include "main.h"
 
 /**
- * main - print Holberton, followed by new line
+ * main - print _putchar, followed by new line
  * Return: 0
  */
 
 int main(void)
 {
-	_putchar('_putchar');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
+
 }
