95024278fc6aac3ce5d63fb8229f9b149c03a804
Charles Mwangi <bsclmr696619@spu.ac.ke>
2023-10-11 09:48:41 -0400

    add 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,11 @@
 
 int print_last_digit(int n)
 {
-    int last_digit;
+	if (n < 0)
+		n *= -1;
 
-    if (n < 0)
-        n *= -1;
+	_putchar('0' + (n % 10));
 
-    last_digit = n % 10;
-    _putchar('0' + last_digit);
-
-    return last_digit;
+	return (n % 10);
 }
+
