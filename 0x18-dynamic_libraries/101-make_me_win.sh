ccdb6e434c7483b91707398a38d0f09d33493256
Aprime <aprime@aprime98.aprime98>
2024-01-19 23:33:53 +0300

    File arrange

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,7 +1,3 @@
-
-
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/betascribbles/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-
-
-export LD_PRELOAD=/tmp/nrandom.so
+wget -p .. https://raw.githubusercontent.com/Aprime98/alx-low_level_programming/main/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD="$PWD/../libhack.so"
